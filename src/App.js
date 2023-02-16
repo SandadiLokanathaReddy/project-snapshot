2d213d65f8cdfb17cecb695a329ef0817073ffba
Lokanatha Reddy Sandadi <lsandadi@fan-c02cce45md6m.fanatics.com>
2023-02-16 19:16:25 +0530

    first basic build

--- src/App.js
+++ src/App.js
@@ -1,36 +1,16 @@
 import "./App.css";
 import NavScroll from "./Components/NavScroll";
 import GridContainer from "./Components/GridContainer";
-import { useState } from "react";
 
 function App() {
-
-	
-	const [keyword, setKeyword] = useState("")
-	const [searchword, setSearchword] = useState("nature")
-
-	const formChange = (e) => {
-		setKeyword(e.target.value)
-	}
- 
-	const formSubmit = (e) => {
-		e.preventDefault();
-		setSearchword(keyword)
-	}
 	return (
 		<div className="App">
 			<NavScroll />
 			<div className="heading-container">
-				<h1 className="heading">	All Snaps</h1>
-				<form className="search-form" onSubmit={e => formSubmit(e)}>
-					<label htmlFor="search-box" className="form-label">Search using keyword</label>
-					<input id="search-box" type="text" placeholder="search..." className="form-control" onChange={e => formChange(e)} value={keyword} />
-					<button type="submit" className="btn btn-primary">Search</button> 
-				</form>
+				<h1 className="heading"> All Snaps</h1>
 			</div>
-			<div className="grid-container">
-				<GridContainer searchword={searchword}/>
-			</div>
+
+			<GridContainer />
 		</div>
 	);
 }
--- src/Components/GridContainer.js
+++ src/Components/GridContainer.js
@@ -1,45 +1,53 @@
-import Card from "react-bootstrap/Card";
-import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import axios from "axios";
+import { useState, useEffect } from "react";
+import OurCard from "./OurCard";
+import OurForm from "./OurForm";
 
-function GridContainer(props) {
-	let images = []
-	const {searchword} = props
-	const getImages = async () => {
-		await axios.get(`https://api.unsplash.com/search/photos?page=1&query=${searchword}&orientation=squarish&client_id=DcatpZ9T66kFddYG90iGQQ_2EnwEWQBX83oklAEL6rQ`)
-		.then((response) => {
-			images = response.data.results
-			console.log(images)
-		})
-	}
+const GridContainer = (props) => {
+	const [images, setImages] = useState([]);
+	const [keyword, setKeyword] = useState("");
+	const [searchword, setSearchword] = useState("all");
 
-	getImages()
+	const formSubmit = (e) => {
+		e.preventDefault();
+		setSearchword(keyword);
+		setKeyword("");
+	};
 
+	useEffect(() => {
+		axios
+			.get(
+				`https://api.unsplash.com/search/photos?page=1&query=${searchword}&orientation=squarish&client_id=DcatpZ9T66kFddYG90iGQQ_2EnwEWQBX83oklAEL6rQ`
+			)
+			.then((response) => {
+				setImages(response.data.results);
+			})
+			.catch((err) => {
+				console.log("Error happened while fetching images data");
+			});
+	}, [searchword]);
 
 	return (
-		<Row xs={1} md={3} className="g-4">
-			{Array.from({ length: 8 }).map((_, idx) => (
-				<Col>
-					{images.map((val, ind) => {
-						return (
-							<Card>
-								<Card.Img variant="top" src={`val.urls.thumb`} />
-								<Card.Body>
-									<Card.Title>Card title</Card.Title>
-									<Card.Text>
-										This is a longer card with supporting text below
-										as a natural lead-in to additional content. This
-										content is a little bit longer.
-									</Card.Text>
-								</Card.Body>
-							</Card>
-						)
-					})}
-				</Col>
-			))}
-		</Row>
+		<>
+			<OurForm
+				formSubmit={formSubmit}
+				keyword={keyword}
+				setKeyword={setKeyword}
+			/>
+			<div className="grid-container">
+				<Row xs={1} md={3} className="g-4">
+					{images.length === 0 ? (
+						<h2>Loading.....</h2>
+					) : (
+						images.map((val, idx) => {
+							return <OurCard key={idx} imageItem={val} />;
+						})
+					)}
+				</Row>
+			</div>
+		</>
 	);
-}
+};
 
 export default GridContainer;
